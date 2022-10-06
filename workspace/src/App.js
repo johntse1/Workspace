0f7e4680465deea356868a0c210b9c4b4a2dcecf
monkeygobblerkenny <kennyxia2001@gmail.com>
2022-10-05 21:11:06 -0400

    login page
    i worked 10 hours on this

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Login</h1>
+
+        <form>
+          <label for="uname">Username</label>
+          <input type="text" id="uname" name="uname"></input><br/>
+          <label for="uname">Password</label>
+          <input type="text" id="pword" name="pword"></input><br/>
+          <input type="submit" value="Submit"></input>
+        </form>
       </header>
     </div>
   );
