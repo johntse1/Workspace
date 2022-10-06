091b820c55576f3879e183f36db317b61666ea20
jackiewu99 <50283342+jackiewu99@users.noreply.github.com>
2022-10-06 01:09:34 -0400

    simple nav bar and dom router

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Home from './Home'
+import {Route, Link} from 'react-router-dom'; 
+import NavBar from './NavBar';
+import Jobs from './Jobs'
+import Profile from './Profile'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Login</h1>
-
-        <form>
-          <label for="uname">Username</label>
-          <input type="text" id="uname" name="uname"></input><br/>
-          <label for="uname">Password</label>
-          <input type="text" id="pword" name="pword"></input><br/>
-          <input type="submit" value="Submit"></input>
-        </form>
-      </header>
-    </div>
+    <div className = "App">
+      <NavBar />
+    <Route exact path="/home" component={Home} />
+    <Route exact path="/profile" component={Profile} />
+    <Route exact path="/jobs" component={Jobs} />
+  </div>
   );
 }
 
