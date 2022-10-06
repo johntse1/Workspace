1fdb952121150f93a7354b56a8c1b9fcec9a259d
John Tse <jtse63@gmail.com>
2022-10-06 01:24:14 -0400

    Combined merge
    Merged both the routing pages and the login form

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -8,10 +8,18 @@ import Profile from './Profile'
 function App() {
   return (
     <div className = "App">
-      <NavBar />
+      <NavBar/>
     <Route exact path="/home" component={Home} />
     <Route exact path="/profile" component={Profile} />
     <Route exact path="/jobs" component={Jobs} />
+
+    <form>
+          <label for="uname">Username</label>
+          <input type="text" id="uname" name="uname"></input><br/>
+          <label for="uname">Password</label>
+          <input type="text" id="pword" name="pword"></input><br/>
+          <input type="submit" value="Submit"></input>
+        </form>
   </div>
   );
 }
