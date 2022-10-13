0fdca6fafacf830a805c960333a6605f3ce8035e
wilsonchooo <wcho1205@gmail.com>
2022-10-13 00:56:38 -0400

    Register Button

--- workspace/src/pages/Home.js
+++ workspace/src/pages/Home.js
@@ -14,14 +14,14 @@ function Home(){
   const[USER_PASSWORD, setUSER_PASSWORD] = useState('')
   const[JWT_TOKEN,setJWT_TOKEN] = useState('')
   const[SignedIn, setSignedIn] = useState(false)
+  const[SignUp,setSignedUp] = useState(false)
+  const[USER_NAME, setUSER_NAME] = useState('')
 
   let API_BASE_URL = 'https://workspace.onrender.com/api/'
   let API_SIGN_IN_URL = 'users/login'
+  let API_SIGN_UP_URL  = 'users'
   // let navigate = useNavigate();
 
-  const signup=()=>{
-
-  }
   const signin=()=>{
     let url = API_BASE_URL + API_SIGN_IN_URL
     axios.post(url,
@@ -57,20 +57,44 @@ function Home(){
         console.log(response)
       })
   }
+
+  const registerUser=()=>{
+    let url = API_BASE_URL + API_SIGN_IN_URL
+    axios.post(url,
+      {
+        name: USER_NAME,
+        email: USER_EMAIL,
+        password: USER_PASSWORD
+      })
+      .then(function(response){
+        console.log(response)
+        toast.dark('Sign in successful')
+        setSignedIn(true)
+      }).catch(function(error){
+        console.log(error.response.status)
+        if(error.response.status === 400)
+        {
+          toast.warning('Invalid Login')
+        }
+        setSignedIn(false)
+      })
+  }
+
+  
   return (
 
     
     <div className = 'container'>
-
-    {!SignedIn && <div className='form-control'>
-      <label>Sign In</label>
+      <h1> Log in </h1>
+    {(!SignUp && !SignedIn) && <div className='form-control'>
+      <label>Email</label>
       <input type ='text' placeholder='Enter your Email' 
       value={USER_EMAIL} 
       onChange={(e) => setUSER_EMAIL(e.target.value)}
       />
     </div>}
-    
-    {!SignedIn && <div className='form-control'>
+
+    {(!SignUp && !SignedIn) && <div className='form-control'>
       <label>Password</label>
       <input type ='password' placeholder='Enter your Password' 
       value={USER_PASSWORD} 
@@ -80,33 +104,27 @@ function Home(){
 
     {SignedIn && <h1> SIGNED IN AT {USER_EMAIL}</h1>}
 
+
+
     {!SignedIn && <Button color='black' text='Sign in' onClick = {signin}/>}
-  
+
+    {!SignedIn && <Button color='black' text='Register instead' onClick = {registerUser}/>}
+
     <ToastContainer/>
+    <div className="App">
+            <main>
+              <br/><br/>
+              <button onClick={()=> registerPopup(true)}>Register</button>
+              <br/>
+              <button onClick={()=> createPostPopup(true)}>+</button>
+            </main>
+
+            <Popup trigger={register} setTrigger={registerPopup}></Popup>
+            <Popup trigger={createPost} setTrigger={createPostPopup}></Popup>
+          </div> 
     </div>
-  );
-
-    // return(
-    // <div>
-    //     <h1>LOGIN</h1>
-
-        
-    //       <div className="App">
-    //         <main>
-    //           <br/><br/>
-    //           <button onClick={()=> registerPopup(true)}>Register</button>
-    //           <br/>
-    //           <button onClick={()=> createPostPopup(true)}>+</button>
-    //         </main>
-
-    //         <Popup trigger={register} setTrigger={registerPopup}></Popup>
-    //         <Popup trigger={createPost} setTrigger={createPostPopup}></Popup>
-    //       </div> 
-        
-
           
-    // </div>
-    // );
+    );
 }
 
 export default Home;
