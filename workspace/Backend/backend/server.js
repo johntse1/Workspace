da08a961b52bfc02ae7c3aa4b35409345163d931
wilsonchooo <wcho1205@gmail.com>
2022-10-12 22:58:42 -0400

    Update server.js

--- workspace/Backend/backend/server.js
+++ workspace/Backend/backend/server.js
@@ -8,6 +8,9 @@ const port = process.env.PORT || 5000
 
 connectDB()
 const app = express()
+app.use(cors({
+    origin:['*']
+}))
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
@@ -18,7 +21,4 @@ app.use('/api/users',require('./routes/userRoutes'))
 app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
-app.use(cors({
-    origin:['*']
-}))
 
