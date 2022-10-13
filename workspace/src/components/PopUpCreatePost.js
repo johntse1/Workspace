2136ef0f9be227110fcc6f9e66bb66d65025ba27
John Tse <jtse63@gmail.com>
2022-10-12 20:55:58 -0400

    in progress post
    create post in progress, do not change

--- workspace/src/components/PopUpCreatePost.js
+++ workspace/src/components/PopUpCreatePost.js
@@ -1,11 +1,22 @@
 import React from 'react'
 import './Popup.css'
 function PopUpCreatePost(trigger) {
-  return (
-    <div>
-      <h3>Working</h3>
+  return (props.trigger)?(
+    <div className='popup'>
+        <div className='popup-inner'>
+            <button className='close' onClick={()=>props.setTrigger(false)}>close</button>
+            {props.children}
+            <br/><br/>
+            <form>
+                <label for="postName">Title:</label>
+                <input type="text" id="title" name="title"></input>
+
+                <label for="cost">Cost:</label>
+                <input type="number" id="cost" name="cost"></input>
+            </form>
+        </div>
     </div>
-  )
+  ) :"";
 }
 
 export default PopUpCreatePost
