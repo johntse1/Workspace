19deca897c63a04ae6c61579697d6007f3a0bfd2
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2022-10-16 17:02:38 -0400

    profile

--- workspace/src/pages/Profile.js
+++ workspace/src/pages/Profile.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css';
+const prof = ['John Tse','4.5',['Fixing','Cleaning','Making'],'A cool guy',['Good worker','Quick Worker']]
 
-function Profile(){
-    return(
-    <div>
-        <h1>Profile</h1>
-        
-    </div>
-    );
-}
+
+
+function Profile() {
+
+ return(
+    
+    
+        <Tabs>
+            <h1>{prof[0]}({prof[1]})</h1>
+          <TabList>
+            <Tab>Skills</Tab>
+            <Tab>About</Tab>
+            <Tab>Reviews</Tab>
+          </TabList>
+      
+          <TabPanel>
+            <h2>{prof[2].map((item, i) => <div key={i}>{item}</div>)}</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>{prof[3]}</h2>
+          </TabPanel>
+          <TabPanel>
+          <h2>{prof[4].map((item, i) => <div key={i}>{item}</div>)}</h2>
+          </TabPanel>
+        </Tabs>
+      );
+  }
 
 export default Profile;
