a251a7c96bbdc466d5427f6d049812044a4b5901
John Tse <jtse63@gmail.com>
2022-10-19 22:27:06 -0400

    Chat function started
    Started working on chat function. Currently not connected to our backend, but unsure if it'll run locally. Need to find a way to host socket.io

--- workspace/Backend/backend/server.js
+++ workspace/Backend/backend/server.js
@@ -6,7 +6,8 @@ const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require ('./config/db')
 const port = process.env.PORT || 3000
 
-connectDB()
+
+//connectDB()
 const app = express()
 app.use(cors())
 app.use(cors({origin: '*'}))
@@ -21,3 +22,43 @@ app.use(errorHandler)
 app.listen(port, () => console.log(`Server started on port ${port}`))
 console.log('please work cors')
 
+
+//creates the server with socket.io
+const {Server} = require("socket.io")
+const http = require("http");
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors:{
+        //which url is calling/called
+        origin: "http://localhost:3000",
+        methods:["GET","POST"],
+    },
+});
+//server generation
+
+//checks if someone is connected to the server
+io.on("connection",(socket)=>{
+    //logs socketid
+    console.log(`User connected:${socket.id}`);
+
+    //joins the room to data
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+        console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    })
+
+    //send message log
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("receive_message",data);
+    });
+
+    //disconnect
+    socket.on("disconnect",()=>{
+        console.log("User Disconnected",socket.id);
+    });
+});
+
+server.listen(3001, ()=>{
+    console.log("SERVER RUNNING");
+});
--- workspace/src/App.js
+++ workspace/src/App.js
@@ -9,6 +9,14 @@ import { Redirect, Switch,useHistory } from "react-router-dom";
 import Button from './components/Button'
 import CreateJob from './pages/CreateJob'
 
+import Chat from "./pages/Chat";
+import io from 'socket.io-client';
+
+import { useState } from 'react';
+import './App.css';
+//backend connection
+const socket  = io.connect("http://localhost:3000");
+
 function App() {
   let history = useHistory()
   const logout =() => {
@@ -16,6 +24,18 @@ function App() {
     history.push('/')
   }
 
+  const[username,setUsername] = useState("");
+  const[room,setRoom] = useState("");
+  const[showChat,setShowChat] = useState(false);
+
+  //connection button
+  const joinRoom = () =>{
+    if(username!==""&&room!==""){
+      //passes the data from the front end to the backend
+      socket.emit("join_room",room);
+      setShowChat(true);
+    }
+  }
 
   return (
     <div className = "App">
@@ -32,9 +52,33 @@ function App() {
         <Route exact path="/jobs" component={Jobs} />
         <Route exact path="/create" component={CreateJob} />
       </Switch>
-
+    
+      {!showChat ? (
+        <div className="joinChatContainer">
+        <h3>Join A chat</h3>
+        <input
+          type="text"
+          placeholder="John..."
+          onChange={(event)=>{
+            setUsername(event.target.value);
+          }}
+        />
+        <input
+          type="text"
+          placeholder="Room..."
+          onChange={(event)=>{
+            setRoom(event.target.value);
+          }}
+        />
+        <button onClick={joinRoom}>Join A Room</button>
+        </div>
+  
+    ):(
+      <Chat socket={socket} username={username} room={room}/>
+    )}
   </div>
   );
 }
+
 
 export default App;
