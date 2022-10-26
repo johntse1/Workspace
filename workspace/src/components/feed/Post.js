8e6bae2134620f3f244289752f95dae72ac6cda3
jackiewu99 <Jackie.Wu99@myhunter.cuny.edu>
2022-10-26 00:42:37 -0400

    added accepting

--- workspace/src/components/feed/Post.js
+++ workspace/src/components/feed/Post.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import Button from '../Button';
 import {Link} from 'react-router-dom'
+import axios from 'axios'
 
 function Post(props){
     const displayTitle = () =>{
-        console.log(props)
+        console.log(props.post._id)
     }
-
+    const acceptJob = () =>{
+        console.log('button clicked')
+        let token = localStorage.getItem("JWT_TOKEN")
+        axios.post('https://workspace.onrender.com/api/jobs/accept/' + props.post._id,
+            {
+            },{ headers: { "Authorization": `Bearer ${token}` } })   
+        .then( function (response){
+          console.log(response.data)
+        }).catch(function (error){
+          console.log(error.response.status)
+        });
+      }
     return(
         <div key={props.post._id}>
             <h1>{props.post.title}</h1>
@@ -16,7 +28,7 @@ function Post(props){
             <div>{props.post.price}</div>
             <div>{props.post.tags}</div>
             <Button text='Not interested' onClick={displayTitle}></Button>
-            <Button text='I want this job'></Button>
+            <Button text='I want this job' onClick={acceptJob}></Button>
         </div>
     );
 }
