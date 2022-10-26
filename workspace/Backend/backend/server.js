439297d0e04f5b169df3d92f8c9c38a60e4cbb56
John Tse <jtse63@gmail.com>
2022-10-26 00:10:06 -0400

    Chat
    Chat.js was edited

--- workspace/Backend/backend/server.js
+++ workspace/Backend/backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express')
-var cors = require('cors')
+const cors = require('cors')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
