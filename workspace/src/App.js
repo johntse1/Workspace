49465d2588b98f5ec7d7caa593115799602ed089
John Tse <jtse63@gmail.com>
2022-11-24 02:34:13 -0500

    Merge complete
    Deleted prior chat, chat to be added in new page.

--- workspace/src/App.js
+++ workspace/src/App.js
@@ -14,32 +14,11 @@ import UserJobs from './pages/UserJobs'
 import OtherUser from './pages/OtherUser'
 import Review from './pages/Review'
 
-
-import Chat from "./pages/Chat";
-import io from 'socket.io-client';
-
-import { useState } from 'react';
 import './App.css';
-//backend connection
-const socket  = io.connect("http://localhost:3000");
 
 function App() {
   let history = useHistory()
   
-
-  const[username,setUsername] = useState("");
-  const[room,setRoom] = useState("");
-  const[showChat,setShowChat] = useState(false);
-
-  //connection button
-  const joinRoom = () =>{
-    if(username!==""&&room!==""){
-      //passes the data from the front end to the backend
-      socket.emit("join_room",room);
-      setShowChat(true);
-    }
-  }
-
   return (
     <div className = "App">
       <h1>WELCOME TO WORKSPACE</h1>
@@ -59,30 +38,6 @@ function App() {
         <Route exact path="/otherUser" component={OtherUser} />
         <Route exact path="/review" component={Review} />
       </Switch>
-    
-      {!showChat ? (
-        <div className="joinChatContainer">
-        <h3>Join A chat</h3>
-        <input
-          type="text"
-          placeholder="John..."
-          onChange={(event)=>{
-            setUsername(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Room..."
-          onChange={(event)=>{
-            setRoom(event.target.value);
-          }}
-        />
-        <button onClick={joinRoom}>Join A Room</button>
-        </div>
-  
-    ):(
-      <Chat socket={socket} username={username} room={room}/>
-    )}
   </div>
   );
 }
